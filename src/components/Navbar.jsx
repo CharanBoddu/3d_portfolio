699047927d425f80ce34c6662247c6044d96a1ed
Charan1303 <charan.cherry1303@gmail.com>
2023-06-17 19:20:30 -0230

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React, {useEffect, useState } from 'react';
+import { Link } from 'react-router-dom' ;
+
+import {styles} from '../styles';
+import {NavLinks } from '../constants';
+import {logo, menu, close} from '../assets';
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
