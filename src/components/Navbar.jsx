5b32653c039f3555785ed3907a636f1fed96bf5a
Charan1303 <charan.cherry1303@gmail.com>
2023-06-17 20:01:52 -0230

    Added an image in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import React, {useEffect, useState } from 'react';
 import { Link } from 'react-router-dom' ;
 
 import {styles} from '../styles';
-import {NavLinks } from '../constants';
+import {navLinks } from '../constants';
 import {logo, menu, close} from '../assets';
 
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+  
   return (
     <nav
         className={`${styles.paddindX} w-full flex 
@@ -14,7 +16,19 @@ const Navbar = () => {
     >
     <div className='w-full flex justify-between
     items-center max-w-7xl mx-auto'> 
+      <Link
+        to="/"
+        className='flex items-center gap-2'
+        onClick={() => {
+          setActive("")
+          window.scrollTo(0, 0);
+        }}
+      >
+        <img src={logo} alt="logo"
+         className='w-9 h-9 object-contain' />
+      
 
+      </Link>
     </div>
     </nav>
   )
