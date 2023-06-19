4565a83dff99da5398f8a554c1001dee5adcb6fc
Charan1303 <charan.cherry1303@gmail.com>
2023-06-19 09:07:46 -0230

    changing the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,9 @@ const Navbar = () => {
       >
         <img src={logo} alt="logo"
          className='w-9 h-9 object-contain' />
-      
+      <p className="text-white text-[18px]
+      font-bold cursor-pointer">Charan <span
+      className='sm:block hidden'>| Charan Boddu</span></p>
 
       </Link>
     </div>
