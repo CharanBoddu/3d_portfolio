493d093dcc1b254af7aa8a2280f982f4f2edb162
Charan1303 <charan.cherry1303@gmail.com>
2023-06-27 15:18:47 -0230

    removed 360 rotation, will add later

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,24 +1,15 @@
-import React, { Suspense, useEffect, useState, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-  const groupRef = useRef();
-
-  useFrame(({ clock }) => {
-    const rotationSpeed = 0.05;
-    const elapsedTime = clock.getElapsedTime();
-    const rotation = elapsedTime * rotationSpeed;
-
-    groupRef.current.rotation.y = rotation;
-  });
 
   return (
-    <group ref={groupRef}>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -34,7 +25,7 @@ const Computers = ({ isMobile }) => {
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
-    </group>
+    </mesh>
   );
 };
 
@@ -42,16 +33,21 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
+    
     setIsMobile(mediaQuery.matches);
 
+   
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
+    
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
+    
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
@@ -59,7 +55,7 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
