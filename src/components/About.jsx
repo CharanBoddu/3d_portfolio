be948907fe5a4b47d8547cfa36ce4f85f2037cfb
Charan1303 <charan.cherry1303@gmail.com>
2023-06-29 17:07:52 -0230

    Adding the introduction, overview. Change react-tilt to react-parallax-tilt, and install it differently

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import { motion } from 'framer-motion';
+import {styles} from '../styles';
+import {services} from '../constants';
+import {fadeIn, textVariant} from '../utils/motion';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+    
+    </>
   )
 }
 
