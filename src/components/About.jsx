6a04911405bdd8dacf9926a876bec54b42c8027c
Charan1303 <charan.cherry1303@gmail.com>
2023-06-30 21:06:07 -0230

    Change the motion.p

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,17 +9,21 @@ import {fadeIn, textVariant} from '../utils/motion';
 const About = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>Introduction</p>
-      <h2 className={styles.sectionHeadText}>Overview.</h2>
-    </motion.div>
+     <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
     
-    <motion.p
-    variants={fadeIn("","",0.1,1)}
-    >
-      Hello, I'm a skilled computer science student.
-    </motion.p>
-    </>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a skilled computer science student with experience in Java, JavaScript,
+        Python, NodeJS. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+</>
   )
 }
 
