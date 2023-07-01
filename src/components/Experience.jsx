8e6a5699df2c7ad8720d5d925208a11503b8082e
Charan1303 <charan.cherry1303@gmail.com>
2023-07-01 00:57:42 -0230

    changed the company title to bold

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,49 +8,44 @@ import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
-    contentStyle={{
-      background: "#1d1836",
-      color: "#fff"
-    }}
-    contentArrowStyle={{ borderRight: "7px solid #232631" }}
-    date={experience.date}
-    iconStyle={{ background: experience.iconBg }}
-    icon={
-      <div className="flex justify-center
-      items-center w-full h-full">
-        <img
-          src={experience.icon}
-          alt={experience.company_name}
-          className="w-[60%] h-[60%] object-contain"
-        />
-      </div>
-    }
-  >
-    <div>
-      <h3
-        className="text-white text-[24px]
-        font bold"
-      >{experience.title}</h3>
-<p className="text-secondary text-[16px]
-font-semibold" style={{margin: 0}}>
-  {experience.company_name}
-</p>
-
+  contentStyle={{
+    background: "#1d1836",
+    color: "#fff",
+  }}
+  contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+  date={experience.date}
+  iconStyle={{ background: experience.iconBg }}
+  icon={
+    <div className='flex justify-center items-center w-full h-full'>
+      <img
+        src={experience.icon}
+        alt={experience.company_name}
+        className='w-[60%] h-[60%] object-contain'
+      />
     </div>
+  }
+>
+  <div>
+    <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+    <p
+      className='text-secondary text-[16px] font-semibold'
+      style={{ margin: 0 }}
+    >
+      {experience.company_name}
+    </p>
+  </div>
 
-    <ul className="mt-5 list-disc ml-5 space-y-2">
-      {experience.points.map((point, index) => (
-        <li
+  <ul className='mt-5 list-disc ml-5 space-y-2'>
+    {experience.points.map((point, index) => (
+      <li
         key={`experience-point-${index}`}
-        className="text-white-100 text-[14px] pl-1 tracking-wider" >
-        
+        className='text-white-100 text-[14px] pl-1 tracking-wider'
+      >
         {point}
-
-
-        </li>
-      ))}
-    </ul>
-  </VerticalTimelineElement>
+      </li>
+    ))}
+  </ul>
+</VerticalTimelineElement>
 )
 
 const Experience = () => {
