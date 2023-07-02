c05e84306d7fe53e69e490f68a8dac454ea3d276
Charan1303 <charan.cherry1303@gmail.com>
2023-07-02 18:09:25 -0230

    completed the form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,6 +34,56 @@ const Contact = () => {
         </p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
+        <form 
+        ref={formRef}
+        onSubmit={handleSubmit}
+        className="mt-12 flex flex-col gap-8"
+        >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your good name?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your email</span>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows="7"
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <button
+          type="submit"
+          className="bg-tertiary py-3 px-8 outline-none
+          w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+
+
+        </form>
       </motion.div>
 
 
