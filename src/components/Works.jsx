4598ada22ff2182171fd3f4275b1f51815081058
Charan1303 <charan.cherry1303@gmail.com>
2023-07-02 16:20:25 -0230

    EDIT the text

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,6 +18,22 @@ const Works = () => {
           Projects.
         </h2>
       </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-3 text-secondary text-[17px]
+        max-w-3xl leading-[30px]"> 
+        Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+
+
+        </motion.p>
+
+      </div>
     
     </>
   )
