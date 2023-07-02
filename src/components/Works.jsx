3cf2ee7900111284e10a520bf579e5f58a28bd79
Charan1303 <charan.cherry1303@gmail.com>
2023-07-02 16:16:30 -0230

    starting the projects sections

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Tilt from "react-parallax-tilt";
+import {motion} from 'framer-motion';
+import {styles} from '../styles';
+import {github} from '../assets';
+import { SectionWrapper } from "../hoc";
+import {projects} from '../constants';
+import { fadeIn, textVariant } from "../utils/motion";
+
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+     <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          My work
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Projects.
+        </h2>
+      </motion.div>
+    
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");
