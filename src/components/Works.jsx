799754aae5d92f9c3547e4f475798bbaa533baa2
Charan1303 <charan.cherry1303@gmail.com>
2023-07-02 17:26:53 -0230

    added the project titles

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,7 +9,27 @@ import { fadeIn, textVariant } from "../utils/motion";
 const ProjectCard = ({index, name, description, tags, image, source_code_link}) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}> 
-      test
+      <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450 
+      }}
+      className="bg-tertiary p-5 rounded-2xl
+      sm:w-[360px] w-full"
+      >
+
+        <div className="relative w-full h-[230px]">
+          <img 
+          src= {image}
+          alt={name}
+          className="w-full h-full object-cover 
+          rounded-2xl"
+          />
+
+        </div>
+
+      </Tilt>
     </motion.div>
   )
 }
