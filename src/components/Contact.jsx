a5bede624973c77dfa89fe21c10c438a7f7cb2f2
Charan1303 <charan.cherry1303@gmail.com>
2023-07-02 18:12:55 -0230

    adding the extra space for planet model

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,32 +1,32 @@
 import { useState, useRef } from "react";
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import emailjs from '@emailjs/browser';
-import {styles} from '../styles';
+import { styles } from '../styles';
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  const formRef = useRef() ;
+  const formRef = useRef();
   const [form, setForm] = useState({
-      name: '',
-      email: '',
-      message: '',
+    name: '',
+    email: '',
+    message: '',
   });
 
-  const[loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => { }
 
-  const handleSubmit = (e) => {}
-  
-  
+  const handleSubmit = (e) => { }
+
+
   return (
     <div className="xl:mt-12 xl:flex-row
     flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
-      variants={slideIn('left', "tween", 0.2, 1)}
-      className="flex-[0.75] bg-black-100 p-8 
+        variants={slideIn('left', "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 
       rounded-2xl"
       >
         <p className={styles.sectionSubText}>
@@ -34,10 +34,10 @@ const Contact = () => {
         </p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <form 
-        ref={formRef}
-        onSubmit={handleSubmit}
-        className="mt-12 flex flex-col gap-8"
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
         >
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
@@ -76,8 +76,8 @@ const Contact = () => {
           </label>
 
           <button
-          type="submit"
-          className="bg-tertiary py-3 px-8 outline-none
+            type="submit"
+            className="bg-tertiary py-3 px-8 outline-none
           w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
             {loading ? 'Sending...' : 'Send'}
           </button>
@@ -86,7 +86,11 @@ const Contact = () => {
         </form>
       </motion.div>
 
+      <motion.div
+      variants={slideIn('right', "tween", 0.2, 1)}
+      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
 
+      </motion.div>
     </div>
   )
 }
