f15d06af8419bd40fb1704d732e893327f6ac754
Charan1303 <charan.cherry1303@gmail.com>
2023-07-02 22:02:42 -0230

    fixed the contact form bug

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,75 +1,84 @@
-import { useState, useRef } from "react";
-import { motion } from 'framer-motion';
-import emailjs from '@emailjs/browser';
-import { styles } from '../styles';
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
-
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
 
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value })
-  }
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
 
-    emailjs.send('service_qo8gc9b', 'template_mmosvyl',
-      {
-        from_name: form.name,
-        to_name: 'Charan',
-        from_email: form.email,
-        to_email: 'cboddu03@gmail.com',
-        message: form.message,
-      },
-      'q2Q68MHOWQLnM9WFo'
-    ).then(() => {
-      setLoading(false),
-        alert('Thank you, I will get back to you as soon as possible!');
-      setForm({
-        name: '',
-        email: '',
-        message: '',
-      })
+    emailjs
+      .send(
+        "service_qo8gc9b",
+        "template_5v9edjg",
+        {
+          from_name: form.name,
+          to_name: "Charan",
+          from_email: form.email,
+          to_email: "cboddu03@gmail.com",
+          message: form.message,
+        },
+        "q2Q68MHOWQLnM9WFo"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
 
-    }, (error) => {
-      setLoading(false)
-      console.log(error)
-      alert('Something went wrong.')
-    })
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
 
-  }
-
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
 
   return (
-    <div className="xl:mt-12 xl:flex-row
-    flex-col-reverse flex gap-10 overflow-hidden">
+    <div
+      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
       <motion.div
-        variants={slideIn('left', "tween", 0.2, 1)}
-        className="flex-[0.75] bg-black-100 p-8 
-      rounded-2xl"
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
-        <p className={styles.sectionSubText}>
-          Get in touch
-        </p>
+        <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
+          className='mt-12 flex flex-col gap-8'
         >
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
@@ -82,7 +91,6 @@ const Contact = () => {
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your email</span>
             <input
@@ -90,41 +98,39 @@ const Contact = () => {
               name='email'
               value={form.email}
               onChange={handleChange}
-              placeholder="What's your email?"
+              placeholder="What's your web address?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
-
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Message</span>
             <textarea
-              rows="7"
+              rows={7}
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder="What do you want to say?"
+              placeholder='What you want to say?'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
 
           <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 outline-none
-          w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
-            {loading ? 'Sending...' : 'Send'}
+            type='submit'
+            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+          >
+            {loading ? "Sending..." : "Send"}
           </button>
-
-
         </form>
       </motion.div>
 
       <motion.div
-        variants={slideIn('right', "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
         <EarthCanvas />
       </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Contact, "contact")
+export default SectionWrapper(Contact, "contact");
