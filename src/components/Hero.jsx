b827b3060a94a921900a188b1fd7456999553515
Charan1303 <charan.cherry1303@gmail.com>
2023-07-23 01:04:21 -0230

    updating the title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915eff]'>Charan</span>
+            Hi, I'm <span className='text-[#915eff]'>Charan Boddu</span>
           </h1>
 
           <p className={`${styles.heroSubText} mt-2  text-white-100`}>
