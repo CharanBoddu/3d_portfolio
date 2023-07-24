9c938a27bc516b6b9ef7ce1decad32cd171f2274
Charan1303 <charan.cherry1303@gmail.com>
2023-07-24 18:14:23 -0230

    edited the linkedin url

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -18,7 +18,7 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color='#f272c8'
+          color='#FFFFFF'
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
