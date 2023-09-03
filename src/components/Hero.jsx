e17594088c9010306d638bfc41e54b867f6b5fe2
Charan1303 <charan.cherry1303@gmail.com>
2023-09-02 23:59:29 -0230

    name change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Charan</span>
+            Hi, I'm <span className='text-[#915EFF]'>Charan Boddu</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2  text-white-100`}>
             I'm a student who likes to code <br className='sm:block hidden' />
