48d1ad9ff7bff17267ae4c576161b43736e7aa26
Charan1303 <charan.cherry1303@gmail.com>
2023-09-04 00:44:21 -0230

    changing the stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,42 +1,20 @@
-import { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
-
-const Stars = (props) => {
-  const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
-
-  {/* This will make the stars rotate using useFrame*/}
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
-
-  return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-        <PointMaterial
-          transparent
-          color='#FFFFFF'
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
-    </group>
-  );
-};
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stars } from "@react-three/drei";
 
 const StarsCanvas = () => {
   return (
     <div className='w-full h-auto absolute inset-0 z-[-1]'>
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
-          <Stars />
+          <Stars
+            radius={100} // Adjust the radius as needed
+            depth={50} // Adjust the depth as needed
+            count={500} // Reduce the number of stars for performance
+            factor={5} // Adjust the factor for density
+            saturation={0} // No color, just white
+          />
         </Suspense>
-
-        <Preload all />
       </Canvas>
     </div>
   );
