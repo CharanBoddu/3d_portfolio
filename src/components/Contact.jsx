ca5990802f0cbf7d06c0d881d33b260b351343da
Charan1303 <charan.cherry1303@gmail.com>
2023-09-10 00:26:17 -0230

    minor correction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -117,7 +117,7 @@ const Contact = () => {
               name='message'
               value={form.message}
               onChange={handleChange}
-              placeholder='What you want to say?'
+              placeholder='What do you want to say?'
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
