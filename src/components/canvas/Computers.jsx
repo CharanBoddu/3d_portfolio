f6285ca88583ca8cb6a999929abd742e5e8ac7e3
CharanBoddu <charan.cherry1303@gmail.com>
2025-11-10 16:55:35 -0330

    fixed the 3d model overflow

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,10 +1,12 @@
-import React, { Suspense, useEffect, useState, useRef } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF, Center } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
+
+
   const computer = useGLTF("./cool/scene.gltf");
 
   return (
@@ -20,19 +22,12 @@ const Computers = ({ isMobile }) => {
           shadow-mapSize={1024}
         />
         <pointLight intensity={1} />
-        <group
-          rotation={[0, Math.PI / 2 /* adjust to face front */ , 0]}
-          position={isMobile ? [-1, -3.8, -2.2] : [-1.2, -5, -1.5] /* negative x moves left */}
-        >
-          {/* Center recenters geometry so rotations happen around the visual center */}
-          <Center>
-            <primitive
-              object={computer.scene}
-              scale={isMobile ? 0.5 : 0.45}
-              rotation={[0, 0, 0]}
-            />
-          </Center>
-        </group>
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 1 : 2}
+          position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+          rotation={[0, 0, 0]}
+        />
       </mesh>
     </group>
   );
@@ -40,9 +35,7 @@ const Computers = ({ isMobile }) => {
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
-  const [isHoveringCanvas, setIsHoveringCanvas] = useState(false);
-  const controlsRef = useRef();
-  const orbitTarget = isMobile ? [-1, -3.8, -2.2] : [-1.2, -5, -1.5];
+ 
 
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 500px)");
@@ -74,32 +67,27 @@ const ComputersCanvas = () => {
 
 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh" }}
-      onMouseEnter={() => setIsHoveringCanvas(true)}
-      onMouseLeave={() => setIsHoveringCanvas(false)}
-    >
+    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
       <Canvas
         frameloop="demand"
         shadows
         dpr={[1, 2]}
         camera={{ position: [20, 3, 5], fov: 25 }}
         gl={{ preserveDrawingBuffer: true, alpha: true }}
-        style={{ pointerEvents: isHoveringCanvas ? "auto" : "none" }}
       >
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls
-            ref={controlsRef}
-            enableZoom={true}
-            minDistance={7}
-            maxDistance={7.9}
+            enableZoom={false}
             maxPolarAngle={Math.PI / 2}
-            minPolarAngle={0}
-            target={orbitTarget}
+            minPolarAngle={Math.PI / 3}
           />
           <Computers isMobile={isMobile} />
         </Suspense>
+
         <Preload all />
       </Canvas>
+
+
     </div>
   );
 };
